d8dd0d2b782b9b16c16b76537cbce0f37b5841c4
Akash Chaudhary <58812239+cyberlord-007@users.noreply.github.com>
2021-05-13 12:44:04 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (82.94%) - LeetHub

--- valid-perfect-square/valid-perfect-square.cpp
+++ valid-perfect-square/valid-perfect-square.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        int tmp = 1;
-        while (num / tmp > tmp) 
-            tmp++;
-        return (num % tmp == 0 && num / tmp == tmp);
+       long long lo = 1;
+        long long hi = (num / 2) + 1;
+        while (lo <= hi)
+        {
+            long long mid = lo + (hi - lo) / 2;
+            if (mid * mid == num)
+                return true;
+            if (mid * mid < num)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return false; 
     }
 };
