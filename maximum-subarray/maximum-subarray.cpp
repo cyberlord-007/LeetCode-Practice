1fec6c21b8e5f52c7806a80c0c23d07f343c66bc
Akash Chaudhary <58812239+cyberlord-007@users.noreply.github.com>
2021-08-14 16:41:38 +0530

    Time: 8 ms (47.59%), Space: 13.1 MB (52.99%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_curr = nums[0];
+        int max_global = nums[0];
+        for(int i=1;i<nums.size();i++) {
+            max_curr = max(nums[i],nums[i]+max_curr);
+            if(max_curr > max_global)
+                max_global = max_curr;
+        }
+        return max_global;
+    }
+};
